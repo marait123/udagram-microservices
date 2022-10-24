9e72ae26a9961b08f2e678ec55ffee1d8bcde3e7
marait123 <max.ibrahim43@hotmail.com>
2022-10-24 18:02:58 +0200

    added travis and fast-push scripts

---
+++ fast-push.sh
@@ -0,0 +1,12 @@
+docker tag reverseproxy marait123/reverseproxy:v1
+docker push  marait123/reverseproxy:v1
+
+
+docker tag reverseproxy marait123/udagram-api-user:v1
+docker push  marait123/udagram-api-user:v1
+
+docker tag reverseproxy marait123/udagram-api-feed:v1
+docker push  marait123/udagram-api-feed:v1
+
+docker tag reverseproxy marait123/udagram-frontend:v1
+docker push  marait123/udagram-frontend:v1
